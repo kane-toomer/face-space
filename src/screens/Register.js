d418fdb59a18a4331e3cb96b1a3abbd0ed25c56f
Kane Toomer <kanetoomer@yahoo.com>
2022-09-27 21:04:25 -0700

    setting up the repository

---
+++ src/screens/Register.js
@@ -0,0 +1,109 @@
+import React from 'react';
+
+import logo from '../assets/images/mememoji2.svg';
+
+const Register = () => {
+
+    return (
+        <>
+            <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+                <div className="w-full max-w-md space-y-8">
+                    <div>
+                        <img
+                            className="mx-auto h-32 w-auto"
+                            src={logo}
+                            alt="Your Company"
+                        />
+                        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-blue-600">
+                            Register for FaceSpace
+                        </h2>
+                        <p className="mt-2 text-center text-sm text-gray-600">
+                            Connect to your online community by signing in.
+                        </p>
+                    </div>
+                    <div className="mt-8 space-y-6">
+                        <input type="hidden" name="remember" defaultValue="true" />
+                        <div className="-space-y-px rounded-md shadow-sm">
+                            <div>
+                                <label htmlFor="first-name" className="sr-only">
+                                    First name
+                                </label>
+                                <input
+                                    id="first-name"
+                                    name="first-name"
+                                    type="text"
+                                    autoComplete="text"
+                                    required
+                                    className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                    placeholder="First Name"
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="last-name" className="sr-only">
+                                    Last name
+                                </label>
+                                <input
+                                    id="last-name"
+                                    name="last-name"
+                                    type="text"
+                                    autoComplete="text"
+                                    required
+                                    className="relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                    placeholder="Last Name"
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="email-address" className="sr-only">
+                                    Email address
+                                </label>
+                                <input
+                                    id="email-address"
+                                    name="email"
+                                    type="email"
+                                    autoComplete="email"
+                                    required
+                                    className="relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                    placeholder="Email Address"
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="password" className="sr-only">
+                                    Password
+                                </label>
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    required
+                                    className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                    placeholder="Password"
+                                />
+                            </div>
+                        </div>
+
+                        <p className="text-sm font-light text-center">By registering for <span className="font-bold">FaceSpace &copy;</span>, you agree to our <span className="font-medium text-blue-600">Terms and Conditions</span> and <span className="font-medium text-blue-600">Privacy Policy</span>.</p>
+
+                        <div>
+                            <button
+                                type="submit"
+                                className="group relative flex w-full justify-center rounded-md border border-transparent bg-blue-600 py-2 px-4 text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                            >
+                                Register
+                            </button>
+                        </div>
+                        <div className="flex items-center justify-center">
+                            <div className="text-sm">
+                                <a href="/">
+                                    Have an account? <span className="hover:underline font-medium text-blue-600 hover:text-blue-700">Sign In Instead</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Register;
---
+++ src/screens/SignIn.js
@@ -0,0 +1,86 @@
+import React from 'react';
+
+import logo from '../assets/images/mememoji2.svg';
+
+const SignIn = () => {
+
+    return (
+        <>
+            <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+                <div className="w-full max-w-md space-y-8">
+                    <div>
+                        <img
+                            className="mx-auto h-32 w-auto"
+                            src={logo}
+                            alt="Your Company"
+                        />
+                        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-blue-600">
+                            Welcome to FaceSpace
+                        </h2>
+                        <p className="mt-2 text-center text-sm text-gray-600">
+                            Connect to your online community by signing in.
+                        </p>
+                    </div>
+                    <div className="mt-8 space-y-6">
+                        <input type="hidden" name="remember" defaultValue="true" />
+                        <div className="-space-y-px rounded-md shadow-sm">
+                            <div>
+                                <label htmlFor="email-address" className="sr-only">
+                                    Email Address
+                                </label>
+                                <input
+                                    id="email-address"
+                                    name="email"
+                                    type="email"
+                                    autoComplete="email"
+                                    required
+                                    className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                    placeholder="Email Address"
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="password" className="sr-only">
+                                    Password
+                                </label>
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    required
+                                    className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                    placeholder="Password"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="flex items-center justify-center">
+                            <div className="text-sm">
+                                <a href="#" className="font-medium text-blue-600 hover:underline hover:text-blue-700">
+                                    Forgot your password?
+                                </a>
+                            </div>
+                        </div>
+                        <div>
+                            <button
+                                type="submit"
+                                className="group relative flex w-full justify-center rounded-md border border-transparent bg-blue-600 py-2 px-4 text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                            >
+                                Sign in
+                            </button>
+                        </div>
+                        <div className="flex items-center justify-center">
+                            <div className="text-sm">
+                                <a href="/register">
+                                    Need an account? <span className="hover:underline font-medium text-blue-600 hover:text-blue-700">Register Instead</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default SignIn;
